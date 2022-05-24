e007c714200c4978acd9069e05f219ca4169208c
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-05-24 20:05:26 +0200

    Fixed nvim-tree not closing

--- lua/tree.lua
+++ lua/tree.lua
@@ -10,9 +10,17 @@ require'nvim-tree'.setup {
     view = {
         side = 'left',
     },
+    git = {
+        enable = true,
+        ignore = false,
+        timeout = 400,
+    },
 }
-vim.api.nvim_set_keymap("n", "<c-n>", "<cmd>NvimTreeFindFile<cr>", { noremap=true })
-vim.api.nvim_create_autocmd('BufEnter', {
-    command = "if winnr('$') == 1 && bufname() == 'NvimTree' . tabpagenr() | quit | endif",
-    nested = true,
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
 })
