672ee18d509c45a04e14a535fc1b0963fd97a2b3
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-07-10 20:06:59 +0200

    Added emmet_ls

--- lua/lsp/lang/generic.lua
+++ lua/lsp/lang/generic.lua
@@ -2,6 +2,7 @@ local lsp = require 'lspconfig'
 local lsp_util = require 'lsp.util'
 local languages = { 'pyright', 'tsserver', 'eslint', 'gopls', 'ansiblels', 'sumneko_lua', 'rust_analyzer', 'dockerls',
   'yamlls', 'volar', 'ccls', 'omnisharp' }
+
 for _, lang in ipairs(languages) do
   lsp[lang].setup {
     on_attach = lsp_util.OnAttach,
--- lua/lsp/lang/init.lua
+++ lua/lsp/lang/init.lua
@@ -1,1 +1,2 @@
 require 'lsp.lang.generic'
+require 'lsp.lang.emmet'
