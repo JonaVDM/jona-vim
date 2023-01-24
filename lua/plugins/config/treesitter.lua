ce239b6d5b550df47264437a7b9fb7458f88f23c
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-01-24 11:49:29 +0100

    Added svelte to lsp

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,10 +1,11 @@
 local treesitter = require 'nvim-treesitter.configs'
 
 treesitter.setup({
-  ensure_installed = { 'org', 'lua', 'rust', 'java', 'javascript', 'typescript', 'go' },
+  ensure_installed = { 'org', 'lua', 'rust', 'java', 'javascript', 'typescript', 'go', 'svelte' },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = { 'org' },
+    -- additional_vim_regex_highlighting = { 'org' },
+    additional_vim_regex_highlighting = true,
   },
   endwise = {
     enable = true,
