92db9ef92f8623126cd5f4a19c6ac4d7a5ebdac3
Jonathan van der Meulen <jonathan.van.der.meulen@enshore.nl>
2024-02-21 11:21:53 +0100

    feat: add command to format json

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,3 +2,6 @@ vim.opt.termguicolors = true
 
 -- removes the text hint
 vim.diagnostic.config({ virtual_text = false })
+
+-- Custom command for quick json formatting - needs jq to be installed
+vim.api.nvim_create_user_command('Json', '%!jq .', {})
