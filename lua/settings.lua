1a9189be647ec12fd67d8d30f31f4d7e793683e8
Jonathan van der Meulen <jonathan.van.der.meulen@enshore.nl>
2024-04-02 18:14:05 +0200

    feat: add go test api client function snippet
    Issue: #30432

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,3 +5,9 @@ vim.diagnostic.config({ virtual_text = false })
 
 -- Custom command for quick json formatting - needs jq to be installed
 vim.api.nvim_create_user_command('Json', '%!jq .', {})
+
+-- auto remove trailing white space from yaml files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern='*.yml',
+  command = "%s/\\s\\+$//e"
+})
