e4f48848823fc1dfdc08df5a3e7b0f624475006e
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-04-14 12:28:10 +0200

    feat: add bool alternate

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -63,6 +63,18 @@ return {
     config = function ()
       require 'plugins.config.doge'
     end
+  },
+  {
+    'rmagatti/alternate-toggler',
+    config = function()
+      require("alternate-toggler").setup {}
+
+      vim.keymap.set(
+        "n",
+        "<leader>a", 
+        "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>"
+      )
+    end
   }
   -- {
   --   'rest-nvim/rest.nvim',
