f90989d97d0b60126fdfb08999aa002c17d7e558
Jonathan van der Meulen <32264711+JonaVDM@users.noreply.github.com>
2024-09-24 11:28:05 +0200

    feat: add nvim tree (filepicker)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,4 +8,30 @@ return {
     end
   }
 
+  -- Filetree
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require 'nvim-tree'.setup {
+        open_on_tab = true,
+        actions = {
+          open_file = {
+            resize_window = true
+          }
+        },
+        view = {
+          adaptive_size = false,
+          side = "left",
+          width = 30,
+        },
+        hijack_cursor = true,
+        sync_root_with_cwd = true,
+      }
+      vim.api.nvim_set_keymap("n", "<c-b>", "<cmd>NvimTreeFindFile<cr>", { noremap = true })
+    end
+  }
+
 }
