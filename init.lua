90349d1478500cd7cffd6b7ae1ccd758f73d711e
Jonathan <jonajvdm@gmail.com>
2024-09-24 20:13:03 +0200

    feat: add some basic settings

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require 'lazy-config'
+require 'settings'
---
+++ lua/settings.lua
@@ -0,0 +1,23 @@
+-- Number/side bar
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = 'yes'
+
+-- spacing
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- width columns
+vim.opt.colorcolumn = '80,120'
+
+-- use mouse
+vim.opt.mouse = 'a'
+
+-- split new panes to the right instead of left
+vim.opt.splitright = true
+
+-- other stuff
+vim.opt.updatetime = 300
+vim.opt.scrolloff = 5
