70b5014c6f5b9f19e690de59ffee0ab31784b331
Jonathan <jonajvdm@gmail.com>
2024-10-08 15:08:03 +0200

    add go package

---
+++ lua/plugins/lang.lua
@@ -0,0 +1,20 @@
+-- an collection of plugins that are useful for one language
+return {
+
+  -- Go(lang)
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()'
+  },
+
+}
